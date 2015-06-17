b0af29d7a9090e883e811aee6ad5c5261c99db81
jacobdlewis <jacobdlewis@gmail.com>
2015-06-17 15:09:34 -0500

    category validations & tests complete

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :name, numericality: false
+  validates :name, uniqueness: true
+  validates :name, format: { without: /[@!$%&*]/, message: "Category name cannot contain any of these characters - @!$%&*" }
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3) }
+    it { should validate_uniqueness_of(:name) }
+  end
+  describe "should be invalid if name isn't formatted correctly" do
+    it { should_not allow_value("MGMT Pl@ns").for(:name) }
+    it { should_not allow_value("").for(:name) }
+    it { should_not allow_value("25").for(:name) }
+  end
+  describe "should be valid with proper name formatting" do
+    it { should allow_value("Management plan").for(:name) }
+  end
 end
