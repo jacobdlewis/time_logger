ed3e615a5cc665f0dec5f4302bde2c7613040598
jacobdlewis <jacobdlewis@gmail.com>
2015-06-21 15:12:24 -0500

    add reporting dashboard view for user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      @user = User.find(current_user.id)
+      @time_entries = @user.time_entries.joins(:category).joins(:client)
+    end
   end
 end
