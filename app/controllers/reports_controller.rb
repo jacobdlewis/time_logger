6e19332c7381d741f748269645843495b6d4b623
jacobdlewis <jacobdlewis@gmail.com>
2015-06-22 11:27:41 -0500

    admin graphs show correct names

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
   def show
-    @time_entries = TimeEntry.all.includes(:user).includes(:category).joins(:client)
+    @time_entries = TimeEntry.all.joins(:user).includes(:category).joins(:client)
   end
 end
