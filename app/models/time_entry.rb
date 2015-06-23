f226aa8231474ab95465900f773d5100795f9c8f
jacobdlewis <jacobdlewis@gmail.com>
2015-06-23 11:28:51 -0500

    created filters for retreiving time entries

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -7,4 +7,8 @@ class TimeEntry < ActiveRecord::Base
             presence: true
   validates :client_id, :category_id, :business_mileage,
             :personal_mileage, :hours_worked, numericality: true
+
+  scope :last_month, -> { where(:date => Date.today.beginning_of_month..Date.today.end_of_month) }
+  scope :last_year, -> { where(:date => Date.today.beginning_of_year..Date.today.end_of_year) }
+  scope :last_week, -> { where(:date => Date.today.beginning_of_week..Date.today.end_of_week) }
 end
