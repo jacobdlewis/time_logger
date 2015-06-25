f8757fa56ef221393faf690abc3f78e4ab43a5b9
jacobdlewis <jacobdlewis@gmail.com>
2015-06-25 11:51:35 -0500

    postmark configuration complete

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module TimeLogger
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.delivery_method = :postmark
+    config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
   end
 end
