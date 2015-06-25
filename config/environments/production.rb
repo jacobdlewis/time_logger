c325280e8f9f1a41b7cfed81b3dc5dcb292c0de0
jacobdlewis <jacobdlewis@gmail.com>
2015-06-25 12:15:55 -0500

    updated host settings for PRD mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
+  config.action_mailer.default_url_options = {:host => "https://sfctimelogger.herokuapp.com/" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
