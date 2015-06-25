3134d5cd0348147478f055047fd534b5db67e826
jacobdlewis <jacobdlewis@gmail.com>
2015-06-24 21:17:58 -0500

    comment & area have default value, are no longer required for time_entry model

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,8 +2,7 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   belongs_to :client
-  validates :date, :client_id, :category_id, :comment, :area,
-            :business_mileage, :personal_mileage, :hours_worked,
+  validates :date, :client_id, :category_id, :business_mileage, :personal_mileage, :hours_worked,
             presence: true
   validates :client_id, :category_id, :business_mileage,
             :personal_mileage, :hours_worked, numericality: true
