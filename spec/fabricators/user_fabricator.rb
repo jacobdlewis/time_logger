798417725504d1cf33d4d38803200646458e7b11
jacobdlewis <jacobdlewis@gmail.com>
2015-07-14 23:41:53 -0500

    updated time entry form to accept half-hour increments

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:user) do
   name                { Faker::Name.name }
   email          { Faker::Internet.email }
   username              {Faker::Name.name}
-  password              "password1"
-  password_confirmation "password1"
+  password              "password"
+  password_confirmation "password"
 end
