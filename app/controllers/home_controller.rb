ceb9c97c82d5cd8fea3063e477e1862c6606dfd6
jacobdlewis <jacobdlewis@gmail.com>
2016-01-01 15:56:39 -0600

    logged in check added to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    redirect_to new_user_time_entry_path(current_user)
+    redirect_to new_user_time_entry_path(current_user) if current_user
   end
 end
