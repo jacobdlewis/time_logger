628140332f792f675a8f19f52a6a962ce54a6ddc
jacobdlewis <jacobdlewis@gmail.com>
2016-01-01 18:47:23 -0600

    projects have an active column that is true by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160101235409) do
+ActiveRecord::Schema.define(version: 20160102004456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(version: 20160101235409) do
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.integer  "client_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "category_id"
+    t.boolean  "active",      default: true
   end
 
   create_table "time_entries", force: :cascade do |t|
