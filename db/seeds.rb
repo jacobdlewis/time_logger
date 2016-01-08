6b0c3eda6b5a9cf150fbcb6e6f695e7524845831
jacobdlewis <jacobdlewis@gmail.com>
2016-01-08 02:41:55 -0600

    updated db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
-Fabricate(:user, username: 'admin', name: 'Administrator', email: "admin@gmail.com", admin?: true )
-Fabricate(:user, username: 'johndoe', name: 'John Doe', email: "johndoe@gmail.com", admin?: false )
-Fabricate(:user, username: 'janedoe', name: 'Jane Doe', email: "janedoe@gmail.com", admin?: false )
+#Fabricate(:user, username: 'admin', name: 'Administrator', email: "admin@gmail.com", admin?: true )
+#Fabricate(:user, username: 'johndoe', name: 'John Doe', email: "johndoe@gmail.com", admin?: false )
+#Fabricate(:user, username: 'janedoe', name: 'Jane Doe', email: "janedoe@gmail.com", admin?: false )
 
 #Categories
 Fabricate(:category, name: "Appraisal")
@@ -17,26 +17,26 @@ Fabricate(:activity, name: "Spraying")
 Fabricate(:activity, name: "Recon")
 Fabricate(:activity, name: "Misc")
 
-Fabricate(:client, first_name: "John", last_name: "Brown")
-Fabricate(:client, first_name: "William", last_name: "Morrison")
-Fabricate(:client, first_name: "Amy", last_name: "Gladstone")
+# Fabricate(:client, first_name: "John", last_name: "Brown")
+# Fabricate(:client, first_name: "William", last_name: "Morrison")
+# Fabricate(:client, first_name: "Amy", last_name: "Gladstone")
 
-Fabricate(:project, handle: "Winter 2015", client_id: 3, category_id: 2)
-Fabricate(:project, handle: "Winter 2015", client_id: 1, category_id: 5)
-Fabricate(:project, handle: "Winter 2016", client_id: 2, category_id: 3)
-Fabricate(:project, handle: "Summer 2015", client_id: 1, category_id: 2)
-Fabricate(:project, handle: "Summer 2016", client_id: 3, category_id: 6)
-Fabricate(:project, handle: "Summer 2015", client_id: 1, category_id: 1)
-Fabricate(:project, handle: "Fall 2015", client_id: 1, category_id: 4)
-Fabricate(:project, handle: "Fall 2015", client_id: 3, category_id: 1)
+# Fabricate(:project, handle: "Winter 2015", client_id: 3, category_id: 2)
+# Fabricate(:project, handle: "Winter 2015", client_id: 1, category_id: 5)
+# Fabricate(:project, handle: "Winter 2016", client_id: 2, category_id: 3)
+# Fabricate(:project, handle: "Summer 2015", client_id: 1, category_id: 2)
+# Fabricate(:project, handle: "Summer 2016", client_id: 3, category_id: 6)
+# Fabricate(:project, handle: "Summer 2015", client_id: 1, category_id: 1)
+# Fabricate(:project, handle: "Fall 2015", client_id: 1, category_id: 4)
+# Fabricate(:project, handle: "Fall 2015", client_id: 3, category_id: 1)
 
-#now through one month ago
-def time_rand from = (Time.now - 2700000), to = Time.now
-  Time.at(from + rand * (to.to_f - from.to_f)).strftime('%Y/%m/%d')
-end
+# #now through one month ago
+# def time_rand from = (Time.now - 2700000), to = Time.now
+#   Time.at(from + rand * (to.to_f - from.to_f)).strftime('%Y/%m/%d')
+# end
 
-#Time entries
-800.times do
-  Fabricate(:time_entry, date: time_rand(), user_id: rand(1..3), project_id: rand(1..8), activity_id: rand(1..5))
-end
+# #Time entries
+# 800.times do
+#   Fabricate(:time_entry, date: time_rand(), user_id: rand(1..3), project_id: rand(1..8), activity_id: rand(1..5))
+# end
 
