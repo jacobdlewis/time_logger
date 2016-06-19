46e0ad330c3fffd2726d6854b97e5ecdddca627e
jacobdlewis <jacobdlewis@gmail.com>
2016-06-19 13:04:50 -0500

    Moved commented code out of controller action.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -17,35 +17,37 @@ class ReportsController < ApplicationController
       @user_totals["#{user.name}".to_sym] = @time_entries.where(user_id: user.id).sum(:hours_worked)
     end
   end
-  # def show
-  #   if params[:duration] == "year"
-  #     @time_entries = TimeEntry.unscoped.all.joins(:user).joins(:category).joins(:client).last_year
-  #   elsif params[:duration] == "month"
-  #     @time_entries = TimeEntry.unscoped.all.joins(:user).joins(:category).joins(:client).last_month
-  #   else
-  #     @time_entries = TimeEntry.unscoped.all.joins(:user).joins(:category).joins(:client).last_week
-  #   end
-  #   respond_to do |format|
-  #     format.html
-  #     format.csv do
-  #       headers['Content-Disposition']="attachment; filename=\"time_log_report\""
-  #       headers['Content-Type'] ||= 'text/csv'
-  #     end
-  #   end
-  # end
 
-  # def employees
-  #   @employees = User.all
-  # end
-
-  # def employee_report
-  #   @user = User.find(params[:id])
-  #   if params[:duration] == "year"
-  #     @time_entries = @user.time_entries.joins(:category).joins(:client).last_year
-  #   elsif params[:duration] == "month"
-  #     @time_entries = @user.time_entries.joins(:category).joins(:client).last_month
-  #   else
-  #     @time_entries = @user.time_entries.joins(:category).joins(:client).last_week
-  #   end
-  # end
 end
+
+# def show
+#   if params[:duration] == "year"
+#     @time_entries = TimeEntry.unscoped.all.joins(:user).joins(:category).joins(:client).last_year
+#   elsif params[:duration] == "month"
+#     @time_entries = TimeEntry.unscoped.all.joins(:user).joins(:category).joins(:client).last_month
+#   else
+#     @time_entries = TimeEntry.unscoped.all.joins(:user).joins(:category).joins(:client).last_week
+#   end
+#   respond_to do |format|
+#     format.html
+#     format.csv do
+#       headers['Content-Disposition']="attachment; filename=\"time_log_report\""
+#       headers['Content-Type'] ||= 'text/csv'
+#     end
+#   end
+# end
+
+# def employees
+#   @employees = User.all
+# end
+
+# def employee_report
+#   @user = User.find(params[:id])
+#   if params[:duration] == "year"
+#     @time_entries = @user.time_entries.joins(:category).joins(:client).last_year
+#   elsif params[:duration] == "month"
+#     @time_entries = @user.time_entries.joins(:category).joins(:client).last_month
+#   else
+#     @time_entries = @user.time_entries.joins(:category).joins(:client).last_week
+#   end
+# end
